9deb31dd03c10075d18637184e5e3f8db3f1b81b
codeseeker <secondarycode230651@gmail.com>
2025-11-14 08:00:33 +0500

    added the outline of the methods

---
+++ outlineOfMethods.java
@@ -0,0 +1,32 @@
+public class BankingApp
+{
+  public static void main(String[] args)
+{
+    // Global Variables
+    int[] accountIDs = new int[100];
+    String[] accountNames = new String[100];
+    double[] accountBalances = new double[100];
+    int totalAccounts = 0;
+    
+  }
+  
+  // Main && Menu
+  public void showMenu(){}
+  public void handleChoice(){}
+
+  // Account Management
+  public void createAccount(){}
+  public void viewAccount(){}
+  public void deleteAccount(){}
+  public void listAccount(){} //Useless
+  
+  // Transactions
+  public void deposit(){}
+  public void withdraw(){}
+  public void checkBalance(){}
+
+  // Data Handling
+  public void saveData(){}
+  public void loadData(){}
+  public int findAccountIndex(){}
+}
